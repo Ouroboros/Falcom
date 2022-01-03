1d786271cc30160f7922dafc7c259b973684b1dd
アリアンロード <Hiromi.Kaede@gmail.com>
2022-01-04 01:44:54 +0800

    [ADD] <Decompiler2/Falcom/ED84> t_voice.tbl

--- Decompiler2/Falcom/ED84/Parser/datatable.py
+++ Decompiler2/Falcom/ED84/Parser/datatable.py
@@ -38,9 +38,63 @@ class BGMTableData(TableDataEntry):
 
         return bytes(body)
 
+class VoiceTableData(TableDataEntry):
+    ENTRY_NAME = 'voice'
+
+    def __init__(self, *, fs: fileio.FileStream = None, **kwargs):
+        super().__init__(fs)
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+        if fs:
+            self.id     = fs.ReadUShort()
+            self.symbol = fs.ReadMultiByte()
+            self.file   = fs.ReadMultiByte()
+            self.word4  = fs.ReadUShort()
+            self.float5 = fs.ReadFloat()
+            self.float6 = fs.ReadFloat()
+            self.word7  = fs.ReadUShort()
+            self.word8  = fs.ReadUShort()
+            self.float9 = fs.ReadFloat()
+            self.text   = fs.ReadMultiByte()
+
+    def toPython(self) -> List[str]:
+        return [
+            f'{self.__class__.__name__}(',
+            f'    id        = 0x{self.id:04X},',
+            f"    symbol    = '{self.symbol}',",
+            f"    file      = '{self.file}',",
+            f'    word4     = {self.word4},',
+            f'    float5    = {self.float5},',
+            f'    float6    = {self.float6},',
+            f'    word7     = {self.word7},',
+            f'    word8     = {self.word8},',
+            f'    float9    = {self.float9},',
+            f"    text      = '{self.text}',",
+            ')',
+        ]
+
+    def serialize(self) -> bytes:
+        body = bytearray()
+
+        body.extend(utils.int_to_bytes(self.id, 2))
+        body.extend(utils.str_to_bytes(self.symbol))
+        body.extend(utils.str_to_bytes(self.file))
+        body.extend(utils.int_to_bytes(self.word4, 2))
+        body.extend(utils.float_to_bytes(self.float5))
+        body.extend(utils.float_to_bytes(self.float6))
+        body.extend(utils.int_to_bytes(self.word7, 2))
+        body.extend(utils.int_to_bytes(self.word8, 2))
+        body.extend(utils.float_to_bytes(self.float9))
+        body.extend(utils.str_to_bytes(self.text))
+
+        return bytes(body)
+
 
 DataTable.DataTableDataTypes.update({
-    'bgm'               : BGMTableData,
+    'bgm'   : BGMTableData,
+    'voice' : VoiceTableData,
 })
 
 DataTable.PythonHeader = [
---
+++ Decompiler2/Falcom/ED84/mod/build.bat
@@ -0,0 +1,57 @@
+@echo off
+cd/d "%~dp0"
+
+set "PATCH_PATH=D:\Game\Steam\steamapps\common\The Legend of Heroes Trails of Cold Steel IV\ouroboros"
+
+if "%~f1"=="" goto:START
+
+"%~f1"
+
+set "filename=%~nx1"
+set "output=%~dpn1.dat"
+set "output_tbl=%~dpn1.tbl"
+
+if /i "%filename:~0,5%" == "alchr" (
+    mkdir "%PATCH_PATH%\scripts\battle\dat\" >NUL 2>NUL
+    move "%output%" "%PATCH_PATH%\scripts\battle\dat\" >NUL
+
+)  else if /i "%filename:~0,3%" == "btl" (
+    mkdir "%PATCH_PATH%\scripts\battle\dat\" >NUL 2>NUL
+    move "%output%" "%PATCH_PATH%\scripts\battle\dat\" >NUL
+
+) else if /i "%filename:~0,3%" == "chr" (
+    mkdir "%PATCH_PATH%\scripts\ani\dat\" >NUL 2>NUL
+    move "%output%" "%PATCH_PATH%\scripts\ani\dat\" >NUL
+
+)  else if /i "%filename:~0,3%" == "rob" (
+    mkdir "%PATCH_PATH%\scripts\ani\dat\" >NUL 2>NUL
+    move "%output%" "%PATCH_PATH%\scripts\ani\dat\" >NUL
+
+) else if /i "%filename:~0,2%" == "t_" (
+    mkdir "%PATCH_PATH%\text\dat\" >NUL 2>NUL
+    move "%output_tbl%" "%PATCH_PATH%\text\dat\" >NUL
+
+) else (
+    mkdir "%PATCH_PATH%\scripts\scena\dat\" >NUL 2>NUL
+    move "%output%" "%PATCH_PATH%\scripts\scena\dat\" >NUL
+)
+
+goto:EOF
+
+:START
+
+call ..\Parser\gen_op.py
+
+for %%i in (
+    alchr*.py
+    t_*.py
+    m????.py
+    a????.py
+    f????.py
+    chr*.py
+    rob*.py
+    btl????.py
+) do (
+    start /B "" /D "%~dp0" cmd /c " "%~f0" "%%~fi" "
+)
+
