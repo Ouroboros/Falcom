0f930e748c1727cb934109beef9498561ff90708
アリアンロード <Hiromi.Kaede@gmail.com>
2022-02-03 00:02:17 +0800

    [ADD] <Decompiler2/Falcom/ED83> 指令参数格式化 ArgReg ArgInt ArgStr ParamStr ParamFloat ParamInt

--- Decompiler2/Falcom/ED84/InstructionTable/utils.py
+++ Decompiler2/Falcom/ED84/InstructionTable/utils.py
@@ -6,14 +6,14 @@ def readAllOperands(ctx: InstructionHandlerContext, fmts: str) -> List[Operand]:
     from .scena import ED84OperandDescriptor
     return ctx.instructionTable.readAllOperands(ctx, ED84OperandDescriptor.fromFormatString(fmts))
 
-def ScriptThread_getFunctionStrWorkValue(threadId: int) -> str:
-    return 'B' + {
-        0x11: 'LB',     # arg reg
-        # 0x22: 'LB',
-        0x33: 'LB',     # arg int
-        0x44: 'LB',     # arg str
-        0x55: 'LB',
-        0xDD: 'S',      # param str
-        0xEE: 'fB',     # param float
-        0xFF: 'LB',     # param long
-    }[threadId]
+# def ScriptThread_getFunctionStrWorkValue(threadId: int) -> str:
+#     return 'B' + {
+#         0x11: 'LB',     # arg reg
+#         # 0x22: 'LB',
+#         0x33: 'LB',     # arg int
+#         0x44: 'LB',     # arg str
+#         0x55: 'LB',
+#         0xDD: 'S',      # param str
+#         0xEE: 'fB',     # param float
+#         0xFF: 'LB',     # param long
+#     }[threadId]
